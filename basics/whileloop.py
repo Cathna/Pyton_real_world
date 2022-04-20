b49b6a27d2d27014697e114d779049903e0d7917
William Caudwell <wilfcaudwell@hotmail.com>
2022-04-20 10:35:48 +0100

    update2

--- basics/whileloop.py
+++ basics/whileloop.py
@@ -8,3 +8,10 @@ username = ''
 
 while username != "pypy":
     username = input("Enter username: ")
+
+while True:
+    username = input("Enter username: ")
+    if username == 'pypy'
+        break
+    else:
+        continue
