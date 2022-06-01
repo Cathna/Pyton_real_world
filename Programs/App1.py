9ce2ea6d21930e2445ec34f6186da055ad15ec2c
William Caudwell <wilfcaudwell@hotmail.com>
2022-06-01 14:19:18 +0100

    update01/06

--- Programs/App1.py
+++ Programs/App1.py
@@ -5,13 +5,31 @@ data = json.load(open("data.json"))
 #the defined function will check the entered word agains the data
 def translate(w):
     w = w.lower()#changes uppercase to lowercase
-    if w in data:
-        return data[w]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+    if w in data:#entry is checked against user entry
+        return data[w]#returns w if in data
+    elif w.title() in data: #if user entered "texas" this will check for "Texas" as well.
+        return data[w.title()]
+    elif w.upper() in data: #in case user enters words like USA or NATO
+        return data[w.upper()]
+    elif len(get_close_matches(w, data.keys())) > 0: #if not in data checks for nearest safe word
+    #displays nearest words to the entered word
+        yn = input ("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        yn = yn.upper()#changes inoput to uppercase
+        if yn == "Y":#runs through options and returns correct response
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn =="N":
+            return "The word does not exist. Please check the word"
+        else:
+            return "We didn't understand your entry"
     else:
         return "The word does not exist. Please check the word"
-
+#Takes output checks it and then displays correctly
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
